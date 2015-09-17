8b527c3d7af334b2bf44f6c3e7cfd8633899a004
Ali Uluğ <keremaliulug@gmail.com>
2015-09-17 13:39:25 +0300

    done

--- YemekSiparisUygulamasi/YemekSiparisMotoru.cs
+++ YemekSiparisUygulamasi/YemekSiparisMotoru.cs
@@ -1,21 +1,59 @@
-﻿namespace YemekSiparisUygulamasi
+﻿using System;
+using System.Linq;
+
+namespace YemekSiparisUygulamasi
 {
 	public class YemekSiparisMotoru
 	{
 		private readonly IRestoranIletisimci _restoranIletisimci;
 		private readonly ISanalPos _sanalPos;
+		private readonly ISiparisRepo _siparisRepo;
+		private readonly ICallCenterIletisimci _callCenterIletisimci;
 
-		public YemekSiparisMotoru(IRestoranIletisimci restoranIletisimci, ISanalPos sanalPos)
+		public YemekSiparisMotoru(IRestoranIletisimci restoranIletisimci, ISanalPos sanalPos, ISiparisRepo siparisRepo, ICallCenterIletisimci callCenterIletisimci)
 		{
 			_restoranIletisimci = restoranIletisimci;
 			_sanalPos = sanalPos;
+			_siparisRepo = siparisRepo;
+			_callCenterIletisimci = callCenterIletisimci;
 		}
 
-		public void SiparisVer(SiparisBilgileri siparisBilgileri)
+		public SiparisSonuc SiparisVer(SiparisBilgileri siparisBilgileri)
 		{
+			_siparisRepo.VeritabaninaKaydet(siparisBilgileri);
+
 			if (siparisBilgileri.OdemeTipi == SiparisOdemeTip.OnlineKrediKarti)
-				_sanalPos.CekimYap(siparisBilgileri.KartBilgileri, siparisBilgileri.ToplamTutar);
+			{
+				bool kartCekimiBasarili =_sanalPos.CekimYap(siparisBilgileri.KartBilgileri, siparisBilgileri.ToplamTutar);
+				_siparisRepo.SiparisCekimBilgisiGuncelle(siparisBilgileri, kartCekimiBasarili);
+				if (!kartCekimiBasarili)
+					return new SiparisSonuc(false);
+			}
 			_restoranIletisimci.SiparisBilgileriniGonder(siparisBilgileri);
-        }
+			return new SiparisSonuc(true);
+		}
+
+		public void BelliBirSuredirCevapAlinamayanSiparisleriIptalEt()
+		{
+			var besDakikadanFazlaBekleyenSiparisler = _siparisRepo.CevapsizSiparisleriAl()
+				.Where(siparis => DateTime.Now.Subtract(siparis.SiparisTarihi).TotalMinutes >= 5);
+			foreach (SiparisBilgileri siparis in besDakikadanFazlaBekleyenSiparisler)
+			{
+				_siparisRepo.SiparisiIptalOlarakKaydet(siparis);
+				_callCenterIletisimci.SiparisIptalBilgisiIlet(siparis);
+				_restoranIletisimci.SiparisIptalIlet(siparis);
+			}
+		}
+
+		public void RestoranCevabiIsle(SiparisBilgileri siparisBilgileri, bool siparisOnaylandimi)
+		{
+			if (siparisOnaylandimi)
+				_siparisRepo.SiparisiOnaylandiOlarakKaydet(siparisBilgileri);
+			else
+			{
+				_siparisRepo.SiparisiIptalOlarakKaydet(siparisBilgileri);
+				_callCenterIletisimci.SiparisIptalBilgisiIlet(siparisBilgileri);
+			}
+		}
 	}
 }
--- YemekSiparisUygulamasi/YemekSiparisMotoruTest.cs
+++ YemekSiparisUygulamasi/YemekSiparisMotoruTest.cs
@@ -1,4 +1,6 @@
-﻿using NSubstitute;
+﻿using System;
+using System.Collections.Generic;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace YemekSiparisUygulamasi
@@ -8,6 +10,8 @@ namespace YemekSiparisUygulamasi
 	{
 		private IRestoranIletisimci _restoranIletisimci;
 		private ISanalPos _sanalPos;
+		private ISiparisRepo _siparisRepo;
+		private ICallCenterIletisimci _callCenterIletisimci;
 		private YemekSiparisMotoru _siparisMotoru;
 		private SiparisBilgileri _odemesizSiparisBilgileri;
 		private SiparisBilgileri _odemeliSiparisBilgileri;
@@ -17,7 +21,9 @@ namespace YemekSiparisUygulamasi
 		{
 			_restoranIletisimci = Substitute.For<IRestoranIletisimci>();
 			_sanalPos = Substitute.For<ISanalPos>();
-			_siparisMotoru = new YemekSiparisMotoru(_restoranIletisimci, _sanalPos);
+			_siparisRepo = Substitute.For<ISiparisRepo>();
+			_callCenterIletisimci = Substitute.For<ICallCenterIletisimci>();
+			_siparisMotoru = new YemekSiparisMotoru(_restoranIletisimci, _sanalPos, _siparisRepo, _callCenterIletisimci);
 			_odemesizSiparisBilgileri = new SiparisBilgileri();
 			_odemeliSiparisBilgileri = new SiparisBilgileri
 			{
@@ -44,11 +50,119 @@ namespace YemekSiparisUygulamasi
 			//then
 			_sanalPos.Received().CekimYap(_odemeliSiparisBilgileri.KartBilgileri, _odemeliSiparisBilgileri.ToplamTutar);
 		}
+
+		[Test]
+		public void kart_cekimi_basarisiz_olursa_hata_doner_restorana_siparis_gecilmez()
+		{
+			_sanalPos
+				.CekimYap(_odemeliSiparisBilgileri.KartBilgileri, _odemeliSiparisBilgileri.ToplamTutar)
+				.Returns(false);
+
+			SiparisSonuc siparisSonucu = _siparisMotoru.SiparisVer(_odemeliSiparisBilgileri);
+
+			Assert.IsFalse(siparisSonucu.SiparisBasarilimi);
+			_restoranIletisimci.DidNotReceive().SiparisBilgileriniGonder(_odemeliSiparisBilgileri);
+		}
+
+		[Test]
+		public void kart_cekimi_basariliysa_basarili_cevap_doner_restorana_siparis_gecilir()
+		{
+			_sanalPos
+				.CekimYap(_odemeliSiparisBilgileri.KartBilgileri, _odemeliSiparisBilgileri.ToplamTutar)
+				.Returns(true);
+
+			SiparisSonuc siparisSonucu = _siparisMotoru.SiparisVer(_odemeliSiparisBilgileri);
+
+			Assert.IsTrue(siparisSonucu.SiparisBasarilimi);
+			_restoranIletisimci.Received().SiparisBilgileriniGonder(_odemeliSiparisBilgileri);
+		}
+
+		[Test]
+		public void siparis_veritabanina_kaydedilir()
+		{
+			_siparisMotoru.SiparisVer(_odemesizSiparisBilgileri);
+
+			_siparisRepo.Received().VeritabaninaKaydet(_odemesizSiparisBilgileri);
+		}
+
+		[Test]
+		public void kart_cekim_sonucu_veritabanina_kaydedilir()
+		{
+			_sanalPos
+				.CekimYap(_odemeliSiparisBilgileri.KartBilgileri, _odemeliSiparisBilgileri.ToplamTutar)
+				.Returns(true);
+
+			_siparisMotoru.SiparisVer(_odemeliSiparisBilgileri);
+
+			_siparisRepo.Received().SiparisCekimBilgisiGuncelle(_odemeliSiparisBilgileri, true);
+		}
+
+		[Test]
+		public void cevapsiz_siparisler_bes_dakika_sonra_iptal_edilir()
+		{
+			List<SiparisBilgileri> cevapsizSiparisler = new List<SiparisBilgileri>
+			{
+				new SiparisBilgileri { SiparisTarihi = DateTime.Now },
+				new SiparisBilgileri { SiparisTarihi = DateTime.Now.AddMinutes(-5) },
+				new SiparisBilgileri { SiparisTarihi = DateTime.Now },
+				new SiparisBilgileri { SiparisTarihi = DateTime.Now.AddMinutes(-6) },
+				new SiparisBilgileri { SiparisTarihi = DateTime.Now },
+			};
+			_siparisRepo.CevapsizSiparisleriAl().Returns(cevapsizSiparisler);
+
+			_siparisMotoru.BelliBirSuredirCevapAlinamayanSiparisleriIptalEt();
+
+			_callCenterIletisimci.Received(2).SiparisIptalBilgisiIlet(Arg.Any<SiparisBilgileri>());
+			_siparisRepo.Received(2).SiparisiIptalOlarakKaydet(Arg.Any<SiparisBilgileri>());
+			_restoranIletisimci.Received(2).SiparisIptalIlet(Arg.Any<SiparisBilgileri>());
+		}
+
+		[Test]
+		public void restoran_siparisi_onaylarsa_veritabanina_kaydet()
+		{
+			_siparisMotoru.RestoranCevabiIsle(_odemesizSiparisBilgileri, true);
+
+			_siparisRepo.Received().SiparisiOnaylandiOlarakKaydet(_odemesizSiparisBilgileri);
+		}
+
+		[Test]
+		public void restorans_siparisi_reddederse_veritabanina_kaydet_call_center_bilgi_ver()
+		{
+			_siparisMotoru.RestoranCevabiIsle(_odemesizSiparisBilgileri, false);
+
+			_siparisRepo.Received().SiparisiIptalOlarakKaydet(_odemesizSiparisBilgileri);
+			_callCenterIletisimci.SiparisIptalBilgisiIlet(_odemesizSiparisBilgileri);
+
+		}
+	}
+
+	public interface ICallCenterIletisimci
+	{
+		void SiparisIptalBilgisiIlet(SiparisBilgileri siparisBilgileri);
+	}
+
+	public interface ISiparisRepo
+	{
+		void VeritabaninaKaydet(SiparisBilgileri siparisBilgileri);
+		void SiparisCekimBilgisiGuncelle(SiparisBilgileri siparisBilgileri, bool kartCekimiBasarilimi);
+		List<SiparisBilgileri> CevapsizSiparisleriAl();
+		void SiparisiIptalOlarakKaydet(SiparisBilgileri siparisBilgileri);
+		void SiparisiOnaylandiOlarakKaydet(SiparisBilgileri siparisBilgileri);
+	}
+
+	public class SiparisSonuc
+	{
+		public SiparisSonuc(bool siparisBasarilimi)
+		{
+			SiparisBasarilimi = siparisBasarilimi;
+		}
+
+		public bool SiparisBasarilimi { get; set; }
 	}
 
 	public interface ISanalPos
 	{
-		void CekimYap(KrediKartBilgileri kartBilgileri, double tutar);
+		bool CekimYap(KrediKartBilgileri kartBilgileri, double tutar);
 	}
 
 	public enum SiparisOdemeTip
@@ -61,6 +175,7 @@ namespace YemekSiparisUygulamasi
 	public interface IRestoranIletisimci
 	{
 		void SiparisBilgileriniGonder(SiparisBilgileri siparisBilgileri);
+		void SiparisIptalIlet(SiparisBilgileri siparisBilgileri);
 	}
 
 	public class SiparisBilgileri
@@ -68,6 +183,7 @@ namespace YemekSiparisUygulamasi
 		public SiparisOdemeTip OdemeTipi { get; set; }
 		public KrediKartBilgileri KartBilgileri { get; set; }
 		public double ToplamTutar { get; set; }
+		public DateTime SiparisTarihi { get; set; }
 	}
 
 	public class KrediKartBilgileri
