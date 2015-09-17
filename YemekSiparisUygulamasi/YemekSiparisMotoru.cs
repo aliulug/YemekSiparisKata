acccd8b067c7522cc4647264ce9a560e600b68c3
Ali Uluğ <keremaliulug@gmail.com>
2015-09-17 11:31:55 +0300

    ilk iki test

---
+++ YemekSiparisUygulamasi/YemekSiparisMotoru.cs
@@ -0,0 +1,21 @@
+﻿namespace YemekSiparisUygulamasi
+{
+	public class YemekSiparisMotoru
+	{
+		private readonly IRestoranIletisimci _restoranIletisimci;
+		private readonly ISanalPos _sanalPos;
+
+		public YemekSiparisMotoru(IRestoranIletisimci restoranIletisimci, ISanalPos sanalPos)
+		{
+			_restoranIletisimci = restoranIletisimci;
+			_sanalPos = sanalPos;
+		}
+
+		public void SiparisVer(SiparisBilgileri siparisBilgileri)
+		{
+			if (siparisBilgileri.OdemeTipi == SiparisOdemeTip.OnlineKrediKarti)
+				_sanalPos.CekimYap(siparisBilgileri.KartBilgileri, siparisBilgileri.ToplamTutar);
+			_restoranIletisimci.SiparisBilgileriniGonder(siparisBilgileri);
+        }
+	}
+}
---
+++ YemekSiparisUygulamasi/YemekSiparisMotoruTest.cs
@@ -0,0 +1,76 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+
+namespace YemekSiparisUygulamasi
+{
+	[TestFixture]
+	public class YemekSiparisMotoruTest
+	{
+		private IRestoranIletisimci _restoranIletisimci;
+		private ISanalPos _sanalPos;
+		private YemekSiparisMotoru _siparisMotoru;
+		private SiparisBilgileri _odemesizSiparisBilgileri;
+		private SiparisBilgileri _odemeliSiparisBilgileri;
+
+		[SetUp]
+		public void her_test_oncesi_calisacak_setup_metodu()
+		{
+			_restoranIletisimci = Substitute.For<IRestoranIletisimci>();
+			_sanalPos = Substitute.For<ISanalPos>();
+			_siparisMotoru = new YemekSiparisMotoru(_restoranIletisimci, _sanalPos);
+			_odemesizSiparisBilgileri = new SiparisBilgileri();
+			_odemeliSiparisBilgileri = new SiparisBilgileri
+			{
+				OdemeTipi = SiparisOdemeTip.OnlineKrediKarti
+			};
+		}
+
+		[Test]
+		public void siparis_geldiginde_ilgili_restorana_yonlendirilir()
+		{
+			//when
+			_siparisMotoru.SiparisVer(_odemesizSiparisBilgileri);
+
+			//then
+			_restoranIletisimci.Received().SiparisBilgileriniGonder(_odemesizSiparisBilgileri);
+		}
+
+		[Test]
+		public void odeme_tipi_online_kart_ise_kart_cekimi_yapilir()
+		{
+			//when
+			_siparisMotoru.SiparisVer(_odemeliSiparisBilgileri);
+
+			//then
+			_sanalPos.Received().CekimYap(_odemeliSiparisBilgileri.KartBilgileri, _odemeliSiparisBilgileri.ToplamTutar);
+		}
+	}
+
+	public interface ISanalPos
+	{
+		void CekimYap(KrediKartBilgileri kartBilgileri, double tutar);
+	}
+
+	public enum SiparisOdemeTip
+	{
+		Nakit,
+		KapidaKrediKarti,
+		OnlineKrediKarti
+	}
+
+	public interface IRestoranIletisimci
+	{
+		void SiparisBilgileriniGonder(SiparisBilgileri siparisBilgileri);
+	}
+
+	public class SiparisBilgileri
+	{
+		public SiparisOdemeTip OdemeTipi { get; set; }
+		public KrediKartBilgileri KartBilgileri { get; set; }
+		public double ToplamTutar { get; set; }
+	}
+
+	public class KrediKartBilgileri
+	{
+	}
+}
